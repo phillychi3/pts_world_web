da0ec8dbd0aa8cab178caf7729535d7c2a86dfd0
phillychi3 <phillychi3@gmail.com>
2023-12-11 02:24:05 +0800

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,51 @@
+<!-- tailwand css -->
+
+<footer class="footer text-center py-2 bg-surface-100-800-token">
+    <div class="flex lg:flex-row flex-col content pt-20 pb-20 mx-auto my-0 justify-between w-4/5 ">
+        <div class="flex-col">
+            <div>
+                <!-- icon here -->
+            </div>
+            <div>
+                PTS world © 2021~2023
+            </div>
+        </div>
+        <div class="flex lg:mt-0 mt-20">
+            <div class="ml-20">
+                <ul>
+                    <div class=" mb-4">
+                        使用條款
+                    </div>
+                    <div class=" mb-4">
+                        使用條款
+                    </div>
+                    <div class=" mb-4">
+                        使用條款
+                    </div>
+                </ul>
+            </div>
+            <div class="ml-20">
+                <div class=" mb-4">
+                    使用條款
+                </div>
+                <div class=" mb-4">
+                    使用條款
+                </div>
+                <div class=" mb-4">
+                    使用條款
+                </div>
+            </div>
+            <div class="ml-20">
+                <div class=" mb-4">
+                    使用條款
+                </div>
+                <div class=" mb-4">
+                    使用條款
+                </div>
+                <div class=" mb-4">
+                    使用條款
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
