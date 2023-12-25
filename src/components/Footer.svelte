1abf00e2e16d5f9b60c9c26ea788768027981f52
phillychi3 <phillychi3@gmail.com>
2023-12-26 02:49:42 +0800

    feat: add privacy and terms

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,6 @@
-<!-- tailwand css -->
+<script>
+    import { base } from '$app/paths'
+</script>
 
 <footer class="footer text-center py-2 bg-surface-100-800-token">
     <div class="flex lg:flex-row flex-col content pt-20 pb-20 mx-auto my-0 justify-between lg:w-4/5 w-full">
@@ -13,6 +15,11 @@
         <div class="flex lg:mt-0 mt-20 text-left sm:flex-row flex-col">
             <div class="ml-20">
                 <ul>
+                    <a href="{base}/">
+                        <div class=" mb-4">
+                            首頁
+                        </div>
+                    </a>
                     <div class=" mb-4">
                         使用教學
                     </div>
@@ -22,12 +29,16 @@
                 </ul>
             </div>
             <div class="ml-20">
-                <div class=" mb-4">
-                    支援伺服器
-                </div>
-                <div class=" mb-4">
-                    Discord TW
-                </div>
+                <a href="https://discord.gg/m6qKNZTaeR">
+                    <div class=" mb-4">
+                        支援伺服器
+                    </div>
+                </a>
+                <a href="https://discordservers.tw/bots/918109428161601566">
+                    <div class=" mb-4">
+                        Discord TW
+                    </div>
+                </a>
                 <div class=" mb-4">
                     Bots.gg
                 </div>
@@ -36,12 +47,16 @@
                 </div>
             </div>
             <div class="ml-20">
-                <div class=" mb-4">
-                    隱私權政策
-                </div>
-                <div class=" mb-4">
-                    使用條款
-                </div>
+                <a href="{base}/privacy">
+                    <div class=" mb-4">
+                        隱私權政策
+                    </div>
+                </a>
+                <a href="{base}/terms">
+                    <div class=" mb-4">
+                        使用條款
+                    </div>
+                </a>
                 <div class=" mb-4">
                     聯絡我們
                 </div>
