b4d979dc19b5ac0f6e5f781d081d7baacb94b2fa
phillychi3 <55632143+phillychi3@users.noreply.github.com>
2024-11-23 07:50:02 +0000

    Deploying to gh-pages from @ phillychi3/pts_world_web@4c5bcd0516c62852cdeb11768b1af03617f65fb2 🚀

---
+++ _app/immutable/chunks/paths.B8XLWPbF.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1kkj94g)==null?void 0:s.base)??"/pts_world_web";var e;const a=((e=globalThis.__sveltekit_1kkj94g)==null?void 0:e.assets)??t;export{a,t as b};
