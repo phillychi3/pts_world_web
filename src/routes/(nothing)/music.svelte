6616b6a5344361cea74ddff55a7c0d01b20aa301
phillychi3 <phillychi3@gmail.com>
2025-02-25 17:14:42 +0800

    fix: update ScrollTrigger import path

--- src/routes/(nothing)/music.svelte
+++ src/routes/(nothing)/music.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import { gsap } from 'gsap'
-	import { ScrollTrigger } from 'gsap/ScrollTrigger'
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js'
 
 	gsap.registerPlugin(ScrollTrigger)
 
